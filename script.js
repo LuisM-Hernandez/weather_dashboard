e525109998407f9b94fdb3be494759d47308e807
Luis <m.luishernandez@outlook.com>
2021-01-07 21:33:22 -0500

    Added five days forecast

--- script.js
+++ script.js
@@ -29,14 +29,38 @@ $(document).ready(function () {
             type: "GET",
             url: "http://api.openweathermap.org/data/2.5/forecast?q="+ searchValue + "&units=imperial"+ "&appid=" + apiKey,
             dataType: "json"
-        }).then(function(response){
-            console.log(response);
+        }).then(function(fiveDays){
+            console.log(fiveDays);
 
-        
+            //Need to fix the icons make them change depending of weather
+
             $("#day1").text(moment().add(1,"days").format("MM/DD/YY"));
             $("<img>").attr("src", "http://openweathermap.org/img/wn/10d@2x.png").appendTo("#day1");
-            $("<div>").text("Temperature: "+ response.list[0].main.temp + " °F").appendTo("#day1");
-            $("<div>").text("Humidity: "+ response.list[0].main.humidity + "%").appendTo("#day1");
+            $("<div>").text("Temperature: "+ fiveDays.list[0].main.temp + " °F").appendTo("#day1");
+            $("<div>").text("Humidity: "+ fiveDays.list[0].main.humidity + "%").appendTo("#day1");
+
+
+            $("#day2").text(moment().add(2,"days").format("MM/DD/YY"));
+            $("<img>").attr("src", "http://openweathermap.org/img/wn/10d@2x.png").appendTo("#day2");
+            $("<div>").text("Temperature: "+ fiveDays.list[7].main.temp + " °F").appendTo("#day2");
+            $("<div>").text("Humidity: "+ fiveDays.list[7].main.humidity + "%").appendTo("#day2");
+
+            $("#day3").text(moment().add(3,"days").format("MM/DD/YY"));
+            $("<img>").attr("src", "http://openweathermap.org/img/wn/10d@2x.png").appendTo("#day3");
+            $("<div>").text("Temperature: "+ fiveDays.list[15].main.temp + " °F").appendTo("#day3");
+            $("<div>").text("Humidity: "+ fiveDays.list[15].main.humidity + "%").appendTo("#day3");
+
+            $("#day4").text(moment().add(4,"days").format("MM/DD/YY"));
+            $("<img>").attr("src", "http://openweathermap.org/img/wn/10d@2x.png").appendTo("#day4");
+            $("<div>").text("Temperature: "+ fiveDays.list[23].main.temp + " °F").appendTo("#day4");
+            $("<div>").text("Humidity: "+ fiveDays.list[23].main.humidity + "%").appendTo("#day4");
+
+            $("#day5").text(moment().add(5,"days").format("MM/DD/YY"));
+            $("<img>").attr("src", "http://openweathermap.org/img/wn/10d@2x.png").appendTo("#day5");
+            $("<div>").text("Temperature: "+ fiveDays.list[31].main.temp + " °F").appendTo("#day5");
+            $("<div>").text("Humidity: "+ fiveDays.list[31].main.humidity + "%").appendTo("#day5");
+
+
 
         })
         
